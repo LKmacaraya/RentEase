b8f90d1decba4eed59539bff5f5d9caca2e86394
Lord Kiann Macaraya <macarayalordkiann@gmail.com>
2025-12-03 18:21:15 +0800

    backend commits

---
+++ backend/node/api/[...all].js
@@ -0,0 +1,8 @@
+import app from '../../src/app.js';
+
+export default function handler(req, res) {
+  if (!req.url.startsWith('/api')) {
+    req.url = `/api${req.url}`;
+  }
+  return app(req, res);
+}
